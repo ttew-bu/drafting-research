10eda4f44176314c5ce11ed6df40b77410282da5
Tristan Tew <ttew@bu.edu>
2022-02-27 17:41:52 -0500

    Add Set from original repo

--- draftbot_sim_adapted.py
+++ draftbot_sim_adapted.py
@@ -350,6 +350,81 @@ class Draft:
                                       column_names=self.card_names,
                                       table_name="cards")
 
+class Set:
+    """A set of cards to draft from.
+
+    Parameters
+    ----------
+    cards: List[Dict[str, object]]
+      Data representing all the cards in the set, created by processing data
+      from mtgjson.com. Each card is represented by a dictionary inside this
+      list. Important keys in the dictionary are:
+        - name: The name of the card.
+        - rarity: The rarity of the card, common, uncommon, rare, or mythic.
+        - colorIdentity: The colors of the card.
+
+    card_names: List[str]
+      A list of card names in the set. This is used here to create a canonical
+      ordering of the cards in the set, since different sources may be
+      inconsistent about the ordering.
+    """
+    def __init__(self, cards, card_names):
+        self.cards = cards
+        self.commons, self.uncommons, self.rares = self.split_by_rarity(cards)
+        self.card_names = card_names
+        self.n_cards = len(self.card_names)
+
+    def random_packs_array(self, n_packs=8, pack_size=14):
+        """Make some random packs of cards from the set. Each pack contains one
+        rare/mytic, thre uncommons, and however many commons are needed to
+        round out the pack.
+
+        Parameters
+        ----------
+        n_packs: int
+          The number of packs to create.
+
+        pack_size: int
+          The number of cards in each pack.
+
+        Returns
+        -------
+        packs: np.array, shape (n_packs, n_cards)
+          The number of each card in each pack.
+        """
+        packs = [self.random_pack_dict(size=pack_size) for _ in range(n_packs)]
+        cards_in_pack_df = pd.DataFrame(np.zeros(shape=(n_packs, self.n_cards), dtype=int),
+                                        columns=self.card_names)
+        for idx, pack in enumerate(packs):
+            for card in pack:
+                name = card['name']
+                cards_in_pack_df.loc[cards_in_pack_df.index[idx], name] += 1
+        return cards_in_pack_df.values
+
+    def random_pack_dict(self, size=14):
+        n_rares, n_uncommons, n_commons = 1, 3, size - 4
+        pack = []
+        for _ in range(n_commons):
+            pack.append(random.choice(self.commons))
+        for _ in range(n_uncommons):
+            pack.append(random.choice(self.uncommons))
+        pack.append(random.choice(self.rares))
+        return pack
+
+    @staticmethod
+    def split_by_rarity(cards):
+        commons, uncommons, rares = [], [], []
+        for card in cards:
+            rarity = card['rarity']
+            if rarity == 'common':
+                commons.append(card)
+            elif rarity == 'uncommon':
+                uncommons.append(card)
+            elif rarity in {'rare', 'mythic'}:
+                rares.append(card)
+        return commons, uncommons, rares
+
+
 class MultiStratDraft:
     """ An adaptation of the Draft class that allows different seats at the draft table
     to use different strategies. The primary difference here is that you must pass in the number of drafters 
